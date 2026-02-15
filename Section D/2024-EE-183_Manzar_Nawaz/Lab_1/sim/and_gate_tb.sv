8f3ea3783ad06eb67b940373de92c62e5011272b
ManzarNawaz <manzarnawaz1911@gmail.com>
2026-02-15 02:53:39 -0800

    tb

---
+++ Section D/2024-EE-183_Manzar_Nawaz/Lab_1/sim/and_gate_tb.sv
@@ -0,0 +1,25 @@
+module and_gate_tb;
+logic a1, b1, y1 ;
+localparam period = 10;
+and_gate UUT (
+    .A(a1),
+    .B(b1),
+    .Y(y1)
+);
+initial 
+begin
+a1 = 0;b1= 0;
+#period;
+a1=0; b1= 1;
+#period;
+a1=1; b1= 0;
+#period;
+a1= 1; b1=1;
+#period;
+$stop;
+end
+initial
+begin
+$monitor("y=%b, a=%b, b= %b", y1,a1,b1);
+end
+endmodule
